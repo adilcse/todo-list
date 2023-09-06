d4867c9636f5957196fb6f60fccbcdae143aacc5
Afsar Ansari <afsaransari110@gmail.com>
2023-09-06 10:39:06 +0530

    app created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="justify-center mr-12  bg-gray-200">
+      <h1 className="text-3xl align-center justify-center text-cyan-400 bg-gray-200 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
